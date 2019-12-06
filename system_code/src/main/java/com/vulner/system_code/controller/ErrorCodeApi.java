94658de68457e52ae9fbef288cb83d09694f71f1
ytwei72 <249422@qq.com>
2019-12-06 10:40:57 +0800

    Add and modify interfaces of error code modules.

--- system_code/src/main/java/com/vulner/system_code/controller/ErrorCodeApi.java
+++ system_code/src/main/java/com/vulner/system_code/controller/ErrorCodeApi.java
@@ -3,10 +3,7 @@ package com.vulner.system_code.controller;
 import com.vulner.common.response.ResponseHelper;
 import com.vulner.system_code.service.ErrorCodeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/sys_code/err_codes")
@@ -14,10 +11,22 @@ public class ErrorCodeApi {
     @Autowired
     ErrorCodeService errorCodeService;
 
-    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    @GetMapping(value = "/all")
     @ResponseBody
-    public Object allCodes() {
+    public Object allErrorList() {
 
         return ResponseHelper.success(errorCodeService.readAll());
+    }
+
+    @GetMapping(value = "/by_code")
+    @ResponseBody
+    public Object errorByCode(@RequestParam("code") String code) {
+        return ResponseHelper.success(ResponseHelper.getErrorByCode(code));
+    }
+
+    @GetMapping(value = "/by_id")
+    @ResponseBody
+    public Object errorById(@RequestParam("id") int id) {
+        return ResponseHelper.success(ResponseHelper.getErrorById(id));
     }
 }
